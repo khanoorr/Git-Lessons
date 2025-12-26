b2130fa57e82af9aa73b79d2b91cbc35c45fe784
Noor Khan <khanoorr3@gmail.com>
2025-12-26 11:01:30 +0530

    features added

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+const { createElement } = require("react");
+
 console.log("Noor Khan");
 function add(a,b) {
     return a + b;
 }
- console.log(add(5, 10));
+console.log(add(5, 10));
+const a = createElement(""h1);
+a.innerHTML = "Hello Sana Suwar";
